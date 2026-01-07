d5e5f93c880032257122eeb526c421e8c9b518b3
FieryRaccoon23 <araib.saleem@gmail.com>
2026-01-07 12:36:53 -0500

    glm submodule added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define SDL_MAIN_HANDLED 1
 #include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
+#include <glm/glm.hpp>
 #include <cstdio>
 
 int main(int argc, char** argv)
@@ -32,5 +33,9 @@ int main(int argc, char** argv)
 
     SDL_DestroyWindow(window);
     SDL_Quit();
+
+    glm::vec2 v(1.0f, 2.0f);
+    std::printf("%f %f\n", v.x, v.y);
+    
     return 0;
 }
