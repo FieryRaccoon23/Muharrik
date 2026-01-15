08bc52940906263dddb30d8dad832574f61d1382
FieryRaccoon23 <araib.saleem@gmail.com>
2026-01-15 12:24:35 -0500

    Allow adding multiple sprite entities

--- src/Game/GameLoop.cpp
+++ src/Game/GameLoop.cpp
@@ -12,7 +12,10 @@ namespace MuharrikGame
     {
         //TEST
         const char* relativePath = "content/engine/test.png";
-        entt::entity e = Muharrik::OnCreateSpriteEntityDelegate(relativePath, 0.0f, 0.0f, 0.0f, 0.5f, 1.0f);
+        entt::entity e1 = Muharrik::OnCreateSpriteEntityDelegate(relativePath, 0.0f, 0.0f, 0.0f, 0.5f, 1.0f);
+
+        const char* relativePath2 = "content/engine/test2.png";
+        entt::entity e2 = Muharrik::OnCreateSpriteEntityDelegate(relativePath2, 250.0f, 0.0f, 0.0f, 0.5f, 1.0f);
     }
 
     void GameLoop::Start()
